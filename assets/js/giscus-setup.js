39f2700fc5aebe60fae301b8909d240545ec97f4
ZhuangYumin <zymx@pm.me>
2025-12-02 10:58:21 +0000

    自动同步更新

---
+++ assets/js/giscus-setup.js
@@ -0,0 +1,42 @@
+function determineGiscusTheme() {
+  
+    let theme =
+      localStorage.getItem("theme") ||
+      document.documentElement.getAttribute("data-theme") ||
+      "system";
+
+    if (theme === "dark") return "dark";
+    if (theme === "light") return "light";
+
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return prefersDark ? "dark" : "light";
+  
+}
+
+(function setupGiscus() {
+  let giscusTheme = determineGiscusTheme();
+
+  let giscusAttributes = {
+    src: "https://giscus.app/client.js",
+    "data-repo": "Astricaelus/MainPageCompiled",
+    "data-repo-id": "R_kgDONtx2zw",
+    "data-category": "Comments",
+    "data-category-id": "DIC_kwDONtx2z84Cm-36",
+    "data-mapping": "pathname",
+    "data-strict": "1",
+    "data-reactions-enabled": "1",
+    "data-emit-metadata": "0",
+    "data-input-position": "top",
+    "data-theme": giscusTheme,
+    "data-lang": "zh-CN",
+    crossorigin: "anonymous",
+    async: true,
+  };
+
+  let giscusScript = document.createElement("script");
+  Object.entries(giscusAttributes).forEach(([key, value]) =>
+    giscusScript.setAttribute(key, value)
+  );
+  document.getElementById("giscus_thread").appendChild(giscusScript);
+})();
+
